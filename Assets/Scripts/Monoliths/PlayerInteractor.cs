0cd9f6503f7e0980aa70987cf2356d348f4e45c6
dumiswa <dumitrudumi605@yahoo.com>
2024-05-09 17:57:12 +0200

    Input System
    PlayerMovement updated
    PlayerInteractor
    Input system

---
+++ Assets/Scripts/Monoliths/PlayerInteractor.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteractor : Monolith
+{
+    private GameObject _player;
+    private LayerMask _interactableLayer;
+    private PlayerProfile _controls;
+
+    public override bool Init()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        if (_player == null)
+        {
+            MonolithStatus = "Couldn't Find Player";
+            return false;
+        }
+
+        _interactableLayer = 1 << LayerMask.NameToLayer("Interactable");
+
+        _controls = new PlayerProfile();
+        _controls.PlayerInteractionMap.Interact.performed += OnInteractPerformed;
+        _controls.PlayerInteractionMap.Interact.Enable();
+
+        return base.Init();
+    }
+
+    private void OnInteractPerformed(InputAction.CallbackContext context) 
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(_player.transform.position, _player.transform.forward, out hit, 2f, _interactableLayer))
+        {
+            InteractWithObject(hit.collider.gameObject);
+        }
+    }
+
+    public void InteractWithObject(GameObject obj)
+    {
+        //Interaction with obj logic
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }
+}
--- Assets/Scripts/Monoliths/PlayerMovement.cs
+++ Assets/Scripts/Monoliths/PlayerMovement.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : Monolith
 {
     private GameObject _player;
     private Rigidbody _rigidbody;
+    private PlayerProfile _controls;
+    private Vector2 _currentMovementInput;
 
     public override bool Init()
     {
@@ -22,7 +25,38 @@ public class PlayerMovement : Monolith
             _rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
         }
 
+        _controls = new PlayerProfile();
+        _controls.PlayerMovemenMap.Move.performed += OnMovementInput;
+        _controls.PlayerMovemenMap.Move.canceled += OnMovementInputCanceled;
+        _controls.PlayerMovemenMap.Move.Enable();
+
         return base.Init();
+    }
+
+    private void OnMovementInput(InputAction.CallbackContext context)
+    {
+        _currentMovementInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnMovementInputCanceled(InputAction.CallbackContext context)
+    {
+        _currentMovementInput = Vector2.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(_currentMovementInput.x, 0, _currentMovementInput.y) * Time.fixedDeltaTime * 5.0f;
+        _rigidbody.MovePosition(_rigidbody.position + move);
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
     }
 
     private void Update()
