d3eb2830e89f3dab1fc358ccafe48133b2a9a95d
dumiswa <dumitrudumi605@yahoo.com>
2024-05-28 20:09:58 +0200

    Cleaned a bit

---
+++ Assets/Scripts/Enemy/WalkingEnemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WalkingEnemy : Enemy
+{
+    public Transform[] waypoints;
+
+    protected override void InitializeStates()
+    {
+        if (waypoints.Length > 0)
+        {
+            Debug.Log("Initializing patrol state");
+            ChangeState(new PatrolState(this, waypoints));
+        }     
+        else Debug.Log("No waypoints set");
+    }
+}
