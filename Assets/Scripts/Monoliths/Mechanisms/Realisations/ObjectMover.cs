23610b091d3fc3dbcc1ee74a12908ffb599a7e92
dumiswa <dumitrudumi605@yahoo.com>
2024-06-05 22:22:03 +0200

    movable ladder

--- Assets/Scripts/Monoliths/Mechanisms/Realisations/ObjectMover.cs
+++ Assets/Scripts/Monoliths/Mechanisms/Realisations/ObjectMover.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Monoliths.Mechanisms
@@ -6,8 +7,10 @@ namespace Monoliths.Mechanisms
     {
         private GameObject _player;
 
-        private Vector3 _initialPlayerPosition;
-        private Vector3 _initialObjectPosition;
+        [SerializeField]
+        private float _distanceThreshold = 0.1f;
+
+        private Vector3 _initialOffset;
         private bool _isMoving = false;
 
         private void Start()
@@ -18,28 +21,42 @@ namespace Monoliths.Mechanisms
 
         public override void Invoke()
         {        
-            if (!_isMoving) 
+            if (!_isMoving)
+            {
+                CalculateInitialOffset();
                 StartMoving();
-            else
-                StopMoving();
+            }
+            else StopMoving();
         }
         public override void Interact(GameObject caller) => Invoke();
-        
-        private void StartMoving()
+       
+        private void CalculateInitialOffset()
         {
-           _initialPlayerPosition = _player.transform.position;
-           _initialObjectPosition = transform.position;
+            Vector3 flatPlayerPosition = new Vector3(_player.transform.position.x, _player.transform.position.y, _player.transform.position.z);
+            Vector3 flatOjectPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 
-           _isMoving = true;
+            Vector3 directionToPlayer = (flatPlayerPosition - flatOjectPosition).normalized;
+            float currentDistance = Vector3.Distance(flatOjectPosition, flatPlayerPosition);
+
+            if (currentDistance <= _distanceThreshold)
+            {
+                transform.position = new Vector3(
+                    flatPlayerPosition.x - directionToPlayer.x * (_distanceThreshold + 0.05f),
+                    transform.position.y,
+                    flatPlayerPosition.z - directionToPlayer.z * (_distanceThreshold + 0.05f));
+            }
+              
+            _initialOffset = transform.position - flatPlayerPosition;
         }
+        private void StartMoving() => _isMoving = true;      
         private void StopMoving() => _isMoving = false;
 
         private void Update()
         {
             if (_isMoving && _player != null)
             {
-                Vector3 playerMovement = _player.transform.position - _initialPlayerPosition;
-                transform.position = _initialObjectPosition + playerMovement;
+                Vector3 newPosition = _player.transform.position + _initialOffset;
+                transform.position = _player.transform.position + _initialOffset;
             }
         }
     }
