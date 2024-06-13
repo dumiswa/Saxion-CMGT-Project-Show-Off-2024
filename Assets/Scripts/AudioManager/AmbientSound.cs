68e8e03acfe76157914d51e57907d67ecc4f6243
dumiswa <dumitrudumi605@yahoo.com>
2024-06-14 00:10:35 +0200

    .

--- Assets/Scripts/AudioManager/AmbientSound.cs
+++ Assets/Scripts/AudioManager/AmbientSound.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 
-public partial class AudioManager
-{
-    [CreateAssetMenu(fileName = "New Ambient Sound", menuName = "Audio/Ambient Sound")]
+[CreateAssetMenu(fileName = "New Ambient Sound", menuName = "Audio/Ambient Sound")]
 public class AmbientSound : ScriptableObject
 {
     public string Name;
     public AudioClip Clip;
     public bool Loop = true;
-}
-
-
 }
 
